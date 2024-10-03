cc0f96e324412c95ca068d42fa9abb266c6d14ff
Sean Talon <avaren@gmail.com>
2024-10-03 20:53:48 +0000

    try again

--- setup.sh
+++ setup.sh
@@ -2,16 +2,16 @@
 
 sudo apt update && sudo apt install -y fish curl
 
-cp -r ./.config/* ~/.config/
-
 # Change the default shell to fish
 if [[ "$SHELL" != "/usr/bin/fish" ]]; then
     echo "Changing default shell to fish..."
     sudo chsh -s /usr/bin/fish $USER
 fi
 
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
+cd ~/.pyenv && src/configure && make -C src
 
 curl -sS https://starship.rs/install.sh | sh -s -- -y
 
-curl https://pyenv.run | bash
+cp -r ./.config/* ~/.config/
