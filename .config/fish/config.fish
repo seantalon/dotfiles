54140733c8455e8c4dd043e091913d61553424ba
Avaren <avaren@gmail.com>
2026-07-22 17:54:52 -0400

    Modify fish config for path and title updates
    Updated fish configuration to include local bin path and modified title function.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,26 +3,14 @@ if status is-interactive
 end
 
 function fish_title
-    # Set the directory length for abbreviation
     set -l short_pwd (fish_prompt_pwd_dir_length=1 prompt_pwd)
-    
-    # Set the window title to "username@hostname: short_pwd"
     echo (whoami)"@"(hostname)": $short_pwd"
 end
-
 
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 
-#set -x PYENV_ROOT "$HOME/.pyenv"
-#if test -d "$PYENV_ROOT/bin"
-#    set -x PATH "$PYENV_ROOT/bin" $PATH
-#end
-#status --is-interactive; and pyenv init --path | source
-
-set PATH $PATH /home/sean/.local/bin
+fish_add_path $HOME/.local/bin
 
 starship init fish | source
-
-pyenv init - | source
